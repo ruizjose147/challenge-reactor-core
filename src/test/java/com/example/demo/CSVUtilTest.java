7df1b6bafc322859345d4d2396e5e5733dfc6614
ruizjose147 <ruizjose_147@hotmail.com>
2022-03-17 22:23:19 -0500

    Se agregan test para funcionamiento de filtarados por edad < 34 y que sean del FC Barcelona y para organizar los jugadores por nacionalidad y rancking

--- src/test/java/com/example/demo/CSVUtilTest.java
+++ src/test/java/com/example/demo/CSVUtilTest.java
@@ -1,22 +1,15 @@
 package com.example.demo;
 
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.*;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class CSVUtilTest {
 
+    private final String club = "FC Barcelona";
     @Test
     void converterData(){
         List<Player> list = CsvUtilFile.getPlayers();
@@ -63,6 +56,47 @@ public class CSVUtilTest {
         assert listFilter.block().size() == 322;
     }
 
+    @Test
+    void reactive_filtrarJugadoresMayoresA34DeLBarcelona() {
+        List<Player> list = CsvUtilFile.getPlayers();
+        Flux<Player> listFlux = Flux.fromStream(list.parallelStream()).cache();
+        Mono<Map<String, Collection<Player>>> listFilter = listFlux
+                .filter(player -> player.age >= 34)
+                .filter(player -> player.club.equals(club))
+                .map(player -> {
+                    player.name = player.name.toUpperCase(Locale.ROOT);
+                    return player;
+                })
+                .buffer(100)
+                .flatMap(playerA -> listFlux
+                        .filter(playerB -> playerA.stream()
+                                .anyMatch(a -> a.club.equals(playerB.club)))
+                )
+                .distinct()
+                .collectMultimap(Player::getClub);
+        System.out.println("hay: " + listFilter.block().size());
+        assert listFilter.block().size() == 0;
+    }
 
+    @Test
+    void judadoresPorNacionalidadYRanking() {
+        List<Player> list = CsvUtilFile.getPlayers();
+        Flux<Player> listFlux = Flux.fromStream(list.parallelStream()).cache();
+        Mono<Map<String, Collection<Player>>> listFilter = listFlux
+                .sort((p1, p2) -> Math.max(p1.getWinners(), p1.getWinners()))
+                .buffer(100)
+                .flatMap(playerA -> listFlux
+                        .filter(playerB -> playerA.stream()
+                                .anyMatch(a -> a.national.equals(playerB.national)))
+                )
+                .distinct()
+                .collectMultimap(Player::getNational);
+
+        listFilter.block().forEach((p, players) -> {
+                    System.out.println("Pais = " + p);
+                    players.stream().forEach(player -> System.out.println("Nombre: " + player.name + "--- Partidos Ganados: " + player.getWinners()));
+                }
+        );
+    }
 
 }
